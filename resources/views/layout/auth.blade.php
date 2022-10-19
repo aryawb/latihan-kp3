2b14476e312496b14e3d747a8252bd8c9dfe03f0
aryawb <61925684+aryawb@users.noreply.github.com>
2022-10-19 17:42:29 +0700

    update pwa
    Upload ke-2 dengan github

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -4,13 +4,19 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="theme-color" content="#13a7ab"/>
 	<title>Login - chat!</title>
+	<!-- PWA  -->
+	<link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+	<link rel="manifest" href="{{ asset('/manifest.json') }}">
 	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
 	<!-- <link href="{{asset('assets/line.css')}}" rel="stylesheet" type="text/css"> -->
 	<link href="{{asset('assets/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
 	<link href="{{asset('assets/css.css')}}" rel="stylesheet" type="text/css">
+
 	<script src="{{asset('assets/jquery-3.6.1.min.js')}}"></script>
+	<script src="{{ asset('/sw.js') }}"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 	<script src="{{asset('assets/bootstrap.bundle.min.js')}}"></script>
 	<script src="{{asset('assets/popper.min.js')}}"></script>
@@ -40,7 +46,11 @@
 		signInButton.addEventListener('click', () => {
 			container.classList.remove("right-panel-active");
 		});
-
+		if (!navigator.serviceWorker.controller) {
+			navigator.serviceWorker.register("/sw.js").then(function (reg) {
+				console.log("Service worker has been registered for scope: " + reg.scope);
+			});
+		}
 	</script>
 </body>
 </html>
